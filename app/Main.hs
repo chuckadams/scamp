12e00b2fc4fc9ef8e4cd1ac8bdf98483777a48f5
Chuck Adams <cja987@gmail.com>
2017-05-16 12:48:43 -0600

    hello world

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Control.Concurrent (threadDelay)
+import Foreign.C.Types
+import SDL.Vect
+import qualified SDL
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 480)
 
 main :: IO ()
-main = someFunc
+main = do
+  SDL.initialize [SDL.InitVideo]
+
+  window <- SDL.createWindow "SDL Tutorial" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+  SDL.showWindow window
+
+  screenSurface <- SDL.getWindowSurface window
+  let white = V4 maxBound maxBound maxBound maxBound
+  SDL.surfaceFillRect screenSurface Nothing white
+  SDL.updateWindowSurface window
+
+  threadDelay 2000000
+
+  SDL.destroyWindow window
+  SDL.quit
